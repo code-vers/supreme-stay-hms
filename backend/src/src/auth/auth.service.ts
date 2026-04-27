3499b5aecdefed5257b3a28219eb3b486718e90e
rakib3719 <bannah76769@gmail.com>
2026-04-27 11:12:03 +0600

    Feat: create user dto

--- backend/src/src/auth/auth.service.ts
+++ backend/src/src/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDao } from './auth.dao';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(private readonly AuthDao: AuthDao) {}
+
+  async register(createAuthDto: CreateAuthDto) {
+    return await this.AuthDao.register(createUserDto);
   }
 
   findAll() {
--- backend/src/src/auth/dto/create-auth.dto.ts
+++ backend/src/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,34 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { UserStatus } from 'src/common/enum/user.enum';
+
 export class CreateAuthDto {}
+
+export class createUserDto {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  first_name!: string;
+
+  @IsString()
+  last_name!: string;
+
+  @IsString()
+  password!: string;
+
+  @IsOptional()
+  @IsString()
+  image!: string;
+
+  @IsString()
+  adress!: string;
+
+  @IsString()
+  phone!: string;
+
+  @IsString()
+  status!: UserStatus;
+
+  @IsString()
+  role_id!: string;
+}
